21aac8bbcc1b4019b5177e6a65319d355ae77af3
qitas <qitas@qitas.cn>
2020-01-17 17:33:42 +0800

    fix

---
+++ src/software/BLE/CyclingSensor/Project/qitas.bat
@@ -0,0 +1,12 @@
+del *.bin /s
+del *.uvopt /s
+del *.uvoptx /s
+del *.uvgui.* /s
+del *.uvguix.* /s
+del *.saved_* /s
+rd obj /s /q
+rd RTE /s /q
+rd list /s /q
+rd listing /s /q
+rd Lst /s /q
+exit
